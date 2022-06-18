810e04a2614ac970e056717ec777bed86a84ca44
Vito Medlej <rip.vitto@gmail.com>
2022-06-18 09:54:40 +0300

    added secret to nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,10 +10,7 @@ export default NextAuth({
     // Configure one or more authentication providers adapter:
     // PrismaAdapter(prisma),
     providers: [CredentialsProvider({
-            // credentials: {     userEmail: {         label: "email",         type: "email"
-            //     },     userPassword: {         label: "Password",         type:
-            // "password"     },     userId: {         label: 'id',         type: 'text' },
-            // id: {         label: 'id',         type: 'text'     } },
+      
             credentials: {
                 userEmail: {
                     label: "email",
@@ -27,12 +24,6 @@ export default NextAuth({
             async authorize(credentials) {
                 try {
 
-                    // const user = { id: 1890, name: "J Smith", email: "jsmith@example.com" } if
-                    // (user) {   // Any object returned will be saved in `user` property of the JWT
-                    //   return user } else {   // If you return null then an error will be
-                    // displayed advising the user to check their details.   return null   // You
-                    // can also Reject this callback with an Error thus the user will be sent to the
-                    // error page with the error message as a query parameter }
 
                     if (!credentials || credentials.userPassword.length < 3 || !credentials
                         ?.userEmail) {
@@ -74,6 +65,7 @@ export default NextAuth({
                 }
             }
         })],
+    secret: process.env.NEXTAUTH_SECRET,
 
     callbacks: {
         jwt: async({token, user}) => {
