aab28d2100e233734e2273115d62c51f0f913eaa
Vito Medlej <rip.vitto@gmail.com>
2022-07-05 19:14:12 +0300

    removed logs

--- src/Functions/signIn.ts
+++ src/Functions/signIn.ts
@@ -11,7 +11,6 @@ const signIn = async (userCreds : TuserCreds) => {
               body: JSON.stringify(userCreds)
         })
         const res = await req.json()
-        console.log('res: ', res);
         if (!res) throw new Error('Failed to log you in')
         return res
     }
